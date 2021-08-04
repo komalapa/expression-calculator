3fa93a492e1c73827b50544b600c7fc064a8bf50
komalapa <komalapa@gmail.com>
2021-08-04 21:35:54 +0400

    feat: all simple tests done

--- src/index.js
+++ src/index.js
@@ -16,39 +16,53 @@ function expressionCalculator(expr) {
     if (bracketRegExp.test(expr)){
         expr.replace(bracketRegExp, expressionCalculator(expr.match(bracketRegExp)))
     } else {
-        const exprArr = expr.split(' ');
+        let exprArr = expr.trim().split(' ');
+
         let mult = exprArr.indexOf('*')
-        while (mult >= 0) {
-            exprArr.splice(mult-1, 3, exprArr[mult-1] * exprArr[mult+1])
-            mult = exprArr.indexOf('*')
-        }
         let div = exprArr.indexOf('/')
-        while (div >= 0) {
-            if (exprArr[div+1] == 0) throw new Error('TypeError: Division by zero.');
-            exprArr.splice(div-1, 3, exprArr[div-1] / exprArr[div+1])
+        while (mult >= 0 || div >= 0) {
+            if (mult >= 0){
+                if(div>=0 && div < mult){
+                    if (exprArr[div+1] == 0) throw new Error('TypeError: Division by zero.');
+                   exprArr.splice(div-1, 3, +exprArr[div-1] / +exprArr[div+1])
+                } else {
+                    exprArr.splice(mult-1, 3, +exprArr[mult-1] * +exprArr[mult+1])        
+                } 
+
+            } else {
+                if (exprArr[div+1] == 0) throw new Error('TypeError: Division by zero.');
+                    exprArr.splice(div-1, 3, +exprArr[div-1] / +exprArr[div+1])
+            }
+            mult = exprArr.indexOf('*')
             div = exprArr.indexOf('/')
+            console.log('*/   ',mult, div, exprArr)
         }
         let sum = exprArr.indexOf('+')
-        while (sum >= 0) {
-            console.log(exprArr[sum])
-            exprArr.splice(sum-1, 3, +exprArr[sum-1] + +exprArr[sum+1])
-            sum = exprArr.indexOf('+')
-        }
         let sub = exprArr.indexOf('-')
-        while (sub>= 0) {
-            exprArr.splice(sub-1, 3, +exprArr[sub-1] - +exprArr[sub+1])
+        while (sum >= 0 || sub >= 0) {
+            if (sum >= 0){
+                if(sub>=0 && sub < sum){
+                   exprArr.splice(sub-1, 3, +exprArr[sub-1] - +exprArr[sub+1])
+                } else {
+                    exprArr.splice(sum-1, 3, +exprArr[sum-1] + +exprArr[sum+1])        
+                } 
+
+            } else {
+                    exprArr.splice(sub-1, 3, +exprArr[sub-1] - +exprArr[sub+1])
+            }
+            sum = exprArr.indexOf('+')
             sub = exprArr.indexOf('-')
+            console.log('+_    ',sum, sub, exprArr)
         }
-        //console.log ('===================',exprArr)
+
         return exprArr[0]
     }
-    //console.log ('===================',exprArr)
     
 
 }
 
-const expr = "2+2";
-const result = 4;
+const expr = "49 * 63 / 58 * 36 ";
+const result = 1916.0690;
 console.log(expressionCalculator(expr))
 module.exports = {
     expressionCalculator
